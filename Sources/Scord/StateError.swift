0039e0c1ef30b69cfccbb9fa2be2830a87b5906e
Vyacheslav Konopkin <robotal76@gmail.com>
2023-01-01 19:33:55 +0300

    Change StateError error visibility

--- Sources/Scord/StateError.swift
+++ Sources/Scord/StateError.swift
@@ -8,7 +8,11 @@
 import Foundation
 
 public struct StateError: Equatable {
-    let error: Error
+    private let error: Error
+
+    public init(_ error: Error) {
+        self.error = error
+    }
 
     public static func ==(lhs: StateError, rhs: StateError) -> Bool {
         type(of: lhs.error) == type(of: rhs.error) &&
