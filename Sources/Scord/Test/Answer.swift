8538fff53d5f4ae6f9bcc760911a4fbea5df7a46
Vyacheslav Konopkin <robotal76@gmail.com>
2023-01-04 00:41:20 +0300

    Add namespace Answer

--- Sources/Scord/Test/Answer.swift
+++ Sources/Scord/Test/Answer.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  Answer.swift
 //  
 //
 //  Created by Vyacheslav Konopkin on 04.01.2023.
@@ -7,23 +7,25 @@
 
 import Combine
 
-public func successAnswer<T>(_ data: T) -> AnyPublisher<T, Error> {
-    Just(data)
-        .setFailureType(to: Error.self)
-        .eraseToAnyPublisher()
-}
+public enum Answer {
+    public static func successAnswer<T>(_ data: T) -> AnyPublisher<T, Error> {
+        Just(data)
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
 
-public func failAnswer<T>(_ error: Error = TestError.someError) -> AnyPublisher<T, Error> {
-    Fail<T, Error>(error: error)
-        .eraseToAnyPublisher()
-}
+    public static func failAnswer<T>(_ error: Error = TestError.someError) -> AnyPublisher<T, Error> {
+        Fail<T, Error>(error: error)
+            .eraseToAnyPublisher()
+    }
 
-public func failAnswer<T>(_ error: Error, _ type: T.Type) -> AnyPublisher<T, Error> {
-    Fail<T, Error>(error: error)
-        .eraseToAnyPublisher()
-}
+    public static func failAnswer<T>(_ error: Error, _ type: T.Type) -> AnyPublisher<T, Error> {
+        Fail<T, Error>(error: error)
+            .eraseToAnyPublisher()
+    }
 
-public func noAnswer<T>() -> AnyPublisher<T, Error> {
-    Empty<T, Error>()
-        .eraseToAnyPublisher()
+    public static func noAnswer<T>() -> AnyPublisher<T, Error> {
+        Empty<T, Error>()
+            .eraseToAnyPublisher()
+    }
 }
