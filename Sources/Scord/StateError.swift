fa9d82304a60b7bed7246997ad91db2b88f750eb
Vyacheslav Konopkin <robotal76@gmail.com>
2023-01-04 21:08:26 +0300

    StateError.description as a computed property

--- Sources/Scord/StateError.swift
+++ Sources/Scord/StateError.swift
@@ -19,12 +19,12 @@ public struct StateError: Equatable {
         self.error = error
     }
 
+    public var description: String {
+        error.localizedDescription
+    }
+
     public static func ==(lhs: StateError, rhs: StateError) -> Bool {
         type(of: lhs.error) == type(of: rhs.error) &&
         lhs.error.localizedDescription == rhs.error.localizedDescription
-    }
-
-    public func description() -> String {
-        error.localizedDescription
     }
 }
